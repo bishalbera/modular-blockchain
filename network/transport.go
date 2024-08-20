94a7cad2207bf3f114eb993b0bb45c758db7c6aa
bishalbera <bishalbera2@gmail.com>
2024-08-20 09:03:15 +0530

    fixed some type issues

--- network/transport.go
+++ network/transport.go
@@ -1,10 +1,12 @@
 package network
 
+import "net"
+
 type NetAddr string
 
 type Transport interface {
 	Consume() <-chan RPC
 	Connect(Transport) error
-	SendMessage(NetAddr, []byte) error
-	Addr() NetAddr
+	SendMessage(net.Addr, []byte) error
+	Addr() net.Addr
 }
